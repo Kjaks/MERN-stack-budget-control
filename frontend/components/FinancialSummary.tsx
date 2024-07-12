884b0b7911e96678ed4a68e758e73dc8ad9ab3a1
Kjaks <karolisjakas04@gmail.com>
2024-07-12 15:31:18 +0200

    final changes code

--- frontend/components/FinancialSummary.tsx
+++ frontend/components/FinancialSummary.tsx
@@ -20,8 +20,8 @@ interface FinancialSummaryProps {
 }
 
 const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, transactions }) => {
-  const [selectedMonth, setSelectedMonth] = useState<string>(''); // Initially no month is selected
-  const [selectedYear, setSelectedYear] = useState<number>(new Date().getFullYear());
+  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
+  const [selectedYear, setSelectedYear] = useState<number | null>(null);
   const [monthsAvailable, setMonthsAvailable] = useState<string[]>([]);
   const [yearsAvailable, setYearsAvailable] = useState<number[]>([]);
   const [chartData, setChartData] = useState<any>({
@@ -29,7 +29,7 @@ const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, 
     datasets: [
       {
         label: 'Financial Distribution',
-        data: [balance, expenses],
+        data: [0, 0],
         backgroundColor: ['#4caf50', '#f44336'],
         hoverBackgroundColor: ['#66bb6a', '#ef5350']
       }
@@ -37,39 +37,52 @@ const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, 
   });
 
   useEffect(() => {
-    // Extract years with avalaible transactions from the list of transactions
+    // Extract all years with transactions from the list of transactions
     const yearsWithTransactions = Array.from(new Set(transactions.map(transaction => new Date(transaction.date).getFullYear())));
     setYearsAvailable(yearsWithTransactions);
+    
+      setSelectedYear(Math.min(...yearsWithTransactions));
   }, [transactions]);
 
   useEffect(() => {
     // Update available months when selected year changes
-    if (selectedYear) {
+    if (selectedYear !== null) {
       const monthsWithTransactions = Array.from(new Set(
         transactions
           .filter(transaction => new Date(transaction.date).getFullYear() === selectedYear)
           .map(transaction => new Date(transaction.date).toLocaleString('default', { month: 'long' }))
       ));
       setMonthsAvailable(monthsWithTransactions);
-      
-      // If no month is selected yet, automatically select the first available month
-      if (!selectedMonth && monthsWithTransactions.length > 0) {
-        setSelectedMonth(monthsWithTransactions[0]);
-      } else {
-        // If the selected month is no longer available for the selected year, select the first available month
-        if (selectedMonth && !monthsWithTransactions.includes(selectedMonth)) {
-          setSelectedMonth(monthsWithTransactions[0]);
-        }
+
+      // If no month is selected yet or selected month is not available, select the first available month
+      if (!selectedMonth || !monthsWithTransactions.includes(selectedMonth)) {
+        setSelectedMonth(monthsWithTransactions.length > 0 ? monthsWithTransactions[0] : null);
       }
     }
   }, [selectedYear, transactions, selectedMonth]);
 
   useEffect(() => {
     // Generate chart data when selected month or year changes
-    if (selectedMonth) {
-      generateChartData();
+    if (selectedYear !== null && selectedMonth !== null) {
+      generateChartData(selectedYear, selectedMonth);
     }
-  }, [selectedMonth, selectedYear, transactions]);
+  }, [selectedYear, selectedMonth, transactions]);
+
+  const generateChartData = (year: number, month: string) => {
+    const { income, expenses } = calculateTotalSavingsMonthly(year, month);
+    
+    setChartData({
+      labels: ['Incomes', 'Expenses'],
+      datasets: [
+        {
+          label: 'Financial Distribution',
+          data: [income, expenses],
+          backgroundColor: ['#4caf50', '#f44336'],
+          hoverBackgroundColor: ['#66bb6a', '#ef5350']
+        }
+      ]
+    });
+  };
 
   const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedMonth(event.target.value);
@@ -80,7 +93,7 @@ const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, 
   };
 
   const calculateTotalSavings = (year: number | null, month: string | null) => {
-    if (year && month === null) {
+    if (year !== null && month === null) {
       const selectedTransactions = transactions.filter(transaction =>
         new Date(transaction.date).getFullYear() === year
       );
@@ -92,7 +105,7 @@ const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, 
         accumulator + (transaction.type === 'expense' ? Math.abs(transaction.amount) : 0), 0);
 
       return totalIncome - totalExpenses;
-    } else if (year && month) { 
+    } else if (year !== null && month !== null) {
       const selectedTransactions = transactions.filter(transaction =>
         new Date(transaction.date).getFullYear() === year &&
         new Date(transaction.date).toLocaleString('default', { month: 'long' }) === month
@@ -128,50 +141,20 @@ const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, 
     };
   };
 
-  const generateChartData = () => {
-    const { income, expenses } = calculateTotalSavingsMonthly(selectedYear, selectedMonth);
-    
-    setChartData({
-      labels: ['Incomes', 'Expenses'],
-      datasets: [
-        {
-          label: 'Financial Distribution',
-          data: [income, expenses],
-          backgroundColor: ['#4caf50', '#f44336'],
-          hoverBackgroundColor: ['#66bb6a', '#ef5350']
-        }
-      ]
-    });
-  };
-
-  const generateMonthOptions = () => {
-    return monthsAvailable.map((month) => (
-      <option key={month} value={month}>
-        {month}
-      </option>
-    ));
-  };
-
-  const generateYearOptions = () => {
-    return yearsAvailable.map((year) => (
-      <option key={year} value={year}>
-        {year}
-      </option>
-    ));
-  };
-
   return (
     <div className="flex flex-col items-center justify-center mt-4">
       <div className="bg-white shadow-md p-4 rounded-lg w-full md:w-3/4 lg:w-1/2">
         <div className="mt-4 text-center">
-          <h1 className="text-3xl font-bold mb-2">
-            Total savings for year {selectedYear}: <span className={calculateTotalSavings(selectedYear, null) < 0 ? 'text-red-500' : 'text-green-500'}>{calculateTotalSavings(selectedYear, null)} $</span>
-          </h1>
+          {selectedYear !== Infinity && (
+            <h1 className="text-3xl font-bold mb-2">
+              Total savings for year {selectedYear}: <span className={calculateTotalSavings(selectedYear, null) < 0 ? 'text-red-500' : 'text-green-500'}>{calculateTotalSavings(selectedYear, null)} $</span>
+            </h1>
+          )}
           <p className="text-3xl font-bold mb-2">
             {selectedMonth && (
               <>
-                Total savings for {selectedMonth}: <span className={calculateTotalSavingsMonthly(selectedYear, selectedMonth).income - calculateTotalSavingsMonthly(selectedYear, selectedMonth).expenses < 0 ? 'text-red-500' : 'text-green-500'}>
-                  {calculateTotalSavingsMonthly(selectedYear, selectedMonth).income - calculateTotalSavingsMonthly(selectedYear, selectedMonth).expenses} $
+                Total savings for {selectedMonth}: <span className={calculateTotalSavings(selectedYear, selectedMonth) < 0 ? 'text-red-500' : 'text-green-500'}>
+                  {calculateTotalSavings(selectedYear, selectedMonth)} $
                 </span>
               </>
             )}
@@ -190,22 +173,30 @@ const FinancialSummary: React.FC<FinancialSummaryProps> = ({ balance, expenses, 
                 <label htmlFor="month-select" className="block text-center">Select a month:</label>
                 <select
                   id="month-select"
-                  value={selectedMonth}
+                  value={selectedMonth || ''}
                   onChange={handleMonthChange}
                   className="block w-full mt-2 border border-gray-300 rounded-md py-2 px-4 bg-white shadow-md"
                 >
-                  {generateMonthOptions()}
+                  {monthsAvailable.map((month) => (
+                    <option key={month} value={month}>
+                      {month}
+                    </option>
+                  ))}
                 </select>
               </div>
               <div className="w-1/2 pl-2">
                 <label htmlFor="year-select" className="block text-center">Select a year:</label>
                 <select
                   id="year-select"
-                  value={selectedYear}
+                  value={selectedYear || ''}
                   onChange={handleYearChange}
                   className="block w-full mt-2 border border-gray-300 rounded-md py-2 px-4 bg-white shadow-md"
                 >
-                  {generateYearOptions()}
+                  {yearsAvailable.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
--- frontend/pages/clientData.tsx
+++ frontend/pages/clientData.tsx
@@ -6,7 +6,7 @@ import PopUp from '../components/PopUp';
 import Image from 'next/image';
 import Link from 'next/link';
 
-// Define the structure of a Transaction object
+// Define el tipo de datos de una transacción
 interface Transaction {
   _id: string;
   userId: string;
@@ -16,11 +16,22 @@ interface Transaction {
   date: string;
 }
 
-// Define the structure of MonthData for financial summaries
+// Define la estructura de MonthData para los resúmenes financieros
 interface MonthData {
   income: number;
   expenses: number;
   savings: number;
+}
+
+// Define la estructura de ChartData para los datos del gráfico
+interface ChartData {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    backgroundColor: string[];
+    hoverBackgroundColor: string[];
+  }[];
 }
 
 const ClientData: React.FC = () => {
@@ -31,15 +42,21 @@ const ClientData: React.FC = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
   const [popupType, setPopupType] = useState<'income' | 'expense' | null>(null);
+  const [yearsAvailable, setYearsAvailable] = useState<number[]>([]);
+  const [monthsAvailable, setMonthsAvailable] = useState<string[]>([]);
+  const [selectedYear, setSelectedYear] = useState<number | null>(null);
+  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
   const [monthData, setMonthData] = useState<MonthData>({ income: 0, expenses: 0, savings: 0 });
   const [annualSavings, setAnnualSavings] = useState<number>(0);
-  const [isEditPopupOpen, setIsEditPopupOpen] = useState<boolean>(false); // Estado para controlar la apertura del pop-up de edición
-  const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null); // Estado para almacenar la transacción seleccionada para editar
+  const [isEditPopupOpen, setIsEditPopupOpen] = useState<boolean>(false);
+  const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null);
+  const [chartData, setChartData] = useState<ChartData | null>(null); // Estado para los datos del gráfico
 
-  // Get the current month
+  // Obtener el mes y el año actual
   const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
 
-  // Fetch user and transaction data on component mount
+  // Cargar datos del usuario y transacciones al montar el componente
   useEffect(() => {
     const storedUserName = localStorage.getItem('userName');
     const storedUserId = localStorage.getItem('userId');
@@ -50,40 +67,7 @@ const ClientData: React.FC = () => {
       axios.get<Transaction[]>(`http://localhost:8000/api/transactions/${storedUserId}`)
         .then(response => {
           const fetchedTransactions = response.data;
-
-          // Calculate balances and expenses
-          let totalBalance = 0;
-          let totalExpenses = 0;
-          let monthIncome = 0;
-          let monthExpenses = 0;
-          let totalIncome = 0;
-
-          fetchedTransactions.forEach((transaction: Transaction) => {
-            const transactionMonth = new Date(transaction.date).getMonth();
-            if (transaction.type === 'income') {
-              totalBalance += transaction.amount;
-              totalIncome += transaction.amount;
-              if (transactionMonth === currentMonth) {
-                monthIncome += transaction.amount;
-              }
-            } else {
-              totalExpenses += Math.abs(transaction.amount);
-              if (transactionMonth === currentMonth) {
-                monthExpenses += Math.abs(transaction.amount);
-              }
-            }
-          });
-
-          const annualSavings = totalIncome - totalExpenses;
-          setTransactions(fetchedTransactions);
-          setBalance(totalBalance);
-          setExpenses(totalExpenses);
-          setMonthData({
-            income: monthIncome,
-            expenses: monthExpenses,
-            savings: monthIncome - monthExpenses,
-          });
-          setAnnualSavings(annualSavings);
+          calculateBalances(fetchedTransactions);
         })
         .catch(error => {
           console.error('Error fetching transactions:', error);
@@ -91,7 +75,83 @@ const ClientData: React.FC = () => {
     }
   }, []);
 
-  // Add a new transaction
+  // Calcular saldos y gastos
+  const calculateBalances = (transactions: Transaction[]) => {
+    let totalBalance = 0;
+    let totalExpenses = 0;
+    let monthIncome = 0;
+    let monthExpenses = 0;
+    let totalIncome = 0;
+  
+    transactions.forEach((transaction: Transaction) => {
+      const transactionDate = new Date(transaction.date);
+      const transactionMonth = transactionDate.getMonth();
+      const transactionYear = transactionDate.getFullYear();
+  
+      if (transaction.type === 'income') {
+        totalBalance += transaction.amount;
+        totalIncome += transaction.amount;
+        if (transactionMonth === currentMonth && transactionYear === currentYear) {
+          monthIncome += transaction.amount;
+        }
+      } else {
+        totalExpenses += Math.abs(transaction.amount);
+        if (transactionMonth === currentMonth && transactionYear === currentYear) {
+          monthExpenses += Math.abs(transaction.amount);
+        }
+      }
+    });
+  
+    const annualSavings = totalIncome - totalExpenses;
+    setTransactions(transactions);
+    setBalance(totalBalance);
+    setExpenses(totalExpenses);
+    setMonthData({
+      income: monthIncome,
+      expenses: monthExpenses,
+      savings: monthIncome - monthExpenses,
+    });
+    setAnnualSavings(annualSavings);
+  
+    // Actualizar años disponibles
+    const yearsWithTransactions = Array.from(new Set(transactions.map(transaction => new Date(transaction.date).getFullYear())));
+    setYearsAvailable(yearsWithTransactions);
+  
+    // Si no hay transacciones, restablecer los estados relacionados
+    if (yearsWithTransactions.length === 0) {
+      setSelectedYear(null);
+      setMonthsAvailable([]);
+      setSelectedMonth(null);
+      setChartData(null); // Asegúrate de limpiar los datos del gráfico si no hay transacciones
+    } else {
+      // Si no hay un año seleccionado o el año seleccionado no está disponible, seleccionar automáticamente el primer año
+      if (!selectedYear || !yearsWithTransactions.includes(selectedYear)) {
+        setSelectedYear(Math.min(...yearsWithTransactions));
+      }
+    }
+  
+    // También restablecer el mes y los datos del gráfico
+    if (yearsWithTransactions.length === 0) {
+      setSelectedYear(null);
+      setSelectedMonth(null);
+      setChartData(null);
+    } else {
+      // Si hay transacciones, asegurarse de que el mes seleccionado sea válido
+      const monthsWithTransactions = Array.from(new Set(
+        transactions
+          .filter(transaction => new Date(transaction.date).getFullYear() === selectedYear)
+          .map(transaction => new Date(transaction.date).toLocaleString('default', { month: 'long' }))
+      ));
+      setMonthsAvailable(monthsWithTransactions);
+  
+      if (!selectedMonth || !monthsWithTransactions.includes(selectedMonth)) {
+        setSelectedMonth(monthsWithTransactions.length > 0 ? monthsWithTransactions[0] : null);
+      }
+    }
+  };
+  
+
+  // Manejar la adición de una nueva transacción
   const handleAddTransaction = (description: string, amount: number, date: string) => {
     const type = popupType === 'income' ? 'income' : 'expense';
 
@@ -105,14 +165,9 @@ const ClientData: React.FC = () => {
     .then(response => {
       const newTransaction = response.data;
 
-      // Update transactions and balances based on the new transaction
-      setTransactions(prevTransactions => [...prevTransactions, newTransaction]);
-
-      if (type === 'income') {
-        setBalance(prevBalance => prevBalance + newTransaction.amount);
-      } else {
-        setExpenses(prevExpenses => prevExpenses + newTransaction.amount);
-      }
+      // Actualizar transacciones y balances basados en la nueva transacción
+      const updatedTransactions = [...transactions, newTransaction];
+      calculateBalances(updatedTransactions);
     })
     .catch(error => {
       console.error('Error adding transaction:', error);
@@ -121,18 +176,18 @@ const ClientData: React.FC = () => {
     setIsPopupOpen(false);
   };
 
-  // Function to handle editing a transaction
+  // Función para manejar la edición de una transacción
   const handleEdit = (id: string) => {
     const transactionToEdit = transactions.find(transaction => transaction._id === id);
     if (transactionToEdit) {
       setSelectedTransaction(transactionToEdit);
-      setIsEditPopupOpen(true); // Ensure setIsEditPopupOpen is called to open the edit popup
+      setIsEditPopupOpen(true);
     } else {
       console.error(`Transaction with ID ${id} not found.`);
     }
   };
 
-  // Handle submission of edited transaction
+  // Manejar la sumisión de la edición de una transacción
   const handleEditSubmission = (description: string, amount: number, date: string) => {
     if (!selectedTransaction) return;
 
@@ -146,48 +201,94 @@ const ClientData: React.FC = () => {
     axios.put<Transaction>(`http://localhost:8000/api/transactions/${selectedTransaction._id}`, updatedTransaction)
       .then(response => {
         const updatedTransaction = response.data;
-        updateTransaction(selectedTransaction._id, updatedTransaction);
-        setIsEditPopupOpen(false); // Close the edit popup after successful update
+        const updatedTransactions = transactions.map(transaction =>
+          transaction._id === selectedTransaction._id ? updatedTransaction : transaction
+        );
+        setTransactions(updatedTransactions);
+        calculateBalances(updatedTransactions);
+        setIsEditPopupOpen(false);
       })
       .catch(error => {
         console.error('Error updating transaction:', error);
       });
   };
 
-  // Function to handle updating a transaction after editing
-  const updateTransaction = (id: string, updatedTransaction: Transaction) => {
-    const updatedTransactions = transactions.map(transaction =>
-      transaction._id === id ? updatedTransaction : transaction
-    );
-    setTransactions(updatedTransactions);
-  };
+// Función para manejar la eliminación de una transacción
+const handleDelete = async (id: string) => {
+  try {
+    await axios.delete(`http://localhost:8000/api/transactions/${id}`);
+    const updatedTransactions = transactions.filter(transaction => transaction._id !== id);
+    calculateBalances(updatedTransactions);
+  } catch (error) {
+    console.error('Error deleting transaction:', error);
+  }
+};
 
-
-  // Function to handle deleting a transaction
-  const handleDelete = async (id: string) => {
-    try {
-      await axios.delete(`http://localhost:8000/api/transactions/${id}`);
-      setTransactions(transactions.filter(transaction => transaction._id !== id));
-
-      // Update balances if necessary
-      const deletedTransaction = transactions.find(transaction => transaction._id === id);
-      if (deletedTransaction) {
-        if (deletedTransaction.type === 'income') {
-          setBalance(prevBalance => prevBalance - deletedTransaction.amount);
-        } else {
-          setExpenses(prevExpenses => prevExpenses - deletedTransaction.amount);
-        }
-      }
-    } catch (error) {
-      console.error('Error deleting transaction:', error);
-    }
-  };
-
-  // Open the popup for adding a transaction
+  // Abrir el popup para añadir una transacción
   const handlePopupOpen = (type: 'income' | 'expense') => {
     setPopupType(type);
     setIsPopupOpen(true);
   };
+
+  // Generar datos del gráfico cuando cambie el año o el mes seleccionado
+  useEffect(() => {
+    if (selectedYear !== null && selectedMonth !== null) {
+      generateChartData(selectedYear, selectedMonth);
+    }
+  }, [selectedYear, selectedMonth]);
+
+  // Generar datos del gráfico basados en el año y mes seleccionados
+  const generateChartData = (year: number, month: string) => {
+    const { income, expenses } = calculateTotalSavingsMonthly(year, month);
+
+    setChartData({
+      labels: ['Ingresos', 'Gastos'],
+      datasets: [
+        {
+          label: 'Distribución Financiera',
+          data: [income, expenses],
+          backgroundColor: ['#4caf50', '#f44336'],
+          hoverBackgroundColor: ['#66bb6a', '#ef5350']
+        }
+      ]
+    });
+  };
+
+  // Calcular ingresos y gastos totales mensuales
+  const calculateTotalSavingsMonthly = (year: number, month: string) => {
+    let totalIncome = 0;
+    let totalExpenses = 0;
+
+    transactions.forEach(transaction => {
+      const transactionDate = new Date(transaction.date);
+      if (transactionDate.getFullYear() === year && transactionDate.toLocaleString('default', { month: 'long' }) === month) {
+        if (transaction.type === 'income') {
+          totalIncome += transaction.amount;
+        } else {
+          totalExpenses += Math.abs(transaction.amount);
+        }
+      }
+    });
+
+    return { income: totalIncome, expenses: totalExpenses };
+  };
+
+  // Manejar cambio de año seleccionado
+  useEffect(() => {
+    if (selectedYear !== null) {
+      const monthsWithTransactions = Array.from(new Set(
+        transactions
+          .filter(transaction => new Date(transaction.date).getFullYear() === selectedYear)
+          .map(transaction => new Date(transaction.date).toLocaleString('default', { month: 'long' }))
+      ));
+      setMonthsAvailable(monthsWithTransactions);
+
+      // Si no hay mes seleccionado o el mes seleccionado no está disponible, seleccionar el primer mes disponible
+      if (!selectedMonth || !monthsWithTransactions.includes(selectedMonth)) {
+        setSelectedMonth(monthsWithTransactions.length > 0 ? monthsWithTransactions[0] : null);
+      }
+    }
+  }, [selectedYear, transactions]);
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-100 to-indigo-200 max-w-full grid grid-cols-3 gap-2 p-1">
@@ -196,7 +297,7 @@ const ClientData: React.FC = () => {
         </div>
         <div className="flex items-center justify-self-stretch col-span-1">
           <h1 className="text-4xl font-bold text-gray-800 text-center pl-12">
-            Hi, {userName}!
+            ¡Hola, {userName}!
           </h1>
         </div>
         <div className="flex items-center justify-end col-span-1">
@@ -218,8 +319,8 @@ const ClientData: React.FC = () => {
 
       <div className="col-span-3 flex items-center justify-center">
         <section className="flex flex-col md:flex-row items-center justify-around gap-4 w-1/2 g-1/2">
-          <button onClick={() => handlePopupOpen('income')} className="bg-green-500 text-white py-3 px-6 rounded-lg shadow hover:bg-green-600 text-center">Add Income</button>
-          <button onClick={() => handlePopupOpen('expense')} className="bg-red-500 text-white py-3 px-6 rounded-lg shadow hover:bg-red-600 text-center">Add Expense</button>
+          <button onClick={() => handlePopupOpen('income')} className="bg-green-500 text-white py-3 px-6 rounded-lg shadow hover:bg-green-600 text-center">Agregar Ingreso</button>
+          <button onClick={() => handlePopupOpen('expense')} className="bg-red-500 text-white py-3 px-6 rounded-lg shadow hover:bg-red-600 text-center">Agregar Gasto</button>
         </section>
       </div>
 
@@ -243,7 +344,7 @@ const ClientData: React.FC = () => {
           onClose={() => setIsEditPopupOpen(false)}
         />
       )}
-      
+
     </div>
   );
 };
